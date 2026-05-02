a57b34b4a707284818620ee0547360a065742b8c
cimorales <cristian.ivan.mg@gmail.com>
2026-05-02 17:13:21 -0600

    Se agrega etiqueta link para agregar icon

--- agenda/includes/layouts/header_client.php
+++ agenda/includes/layouts/header_client.php
@@ -9,8 +9,7 @@ $user = Auth::user();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="robots" content="noindex,nofollow"><!-- panel no debe indexarse -->
   <title><?= e($pageTitle) ?> · BellaNick Clinic</title>
-
-  <link rel="icon" type="image/png" href="https://depilasermexico.com/favicon.ico">
+  <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
   <link rel="preconnect" href="https://fonts.googleapis.com">
